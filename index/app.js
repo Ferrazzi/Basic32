f4225867bfdaf04f117fec61824d1651d2e3f241
Ferrazzi <115821363+Ferrazzi@users.noreply.github.com>
2025-09-05 18:49:04 +0200

    Add files via upload

---
+++ index/app.js
@@ -0,0 +1,4 @@
+// Manifest fisso + console dopo il flash
+const installer = document.getElementById('installer');
+installer.setAttribute('manifest', 'manifests/firmware.json');
+installer.setAttribute('show-console', '');
