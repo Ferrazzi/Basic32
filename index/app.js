f7b626b8a73655eac40bd36becc7df3e14f28bcd
Ferrazzi <115821363+Ferrazzi@users.noreply.github.com>
2025-09-05 18:51:33 +0200

    Delete index directory

--- index/app.js
+++
@@ -1,4 +0,0 @@
-// Manifest fisso + console dopo il flash
-const installer = document.getElementById('installer');
-installer.setAttribute('manifest', 'manifests/firmware.json');
-installer.setAttribute('show-console', '');
